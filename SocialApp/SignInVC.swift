a9232643a4012228ae2c09ff304160bc30187c69
Brian Lee <brianjacoblee@gmail.com>
2016-11-11 20:41:00 -0500

    FacebookAuth

--- SocialApp/SignInVC.swift
+++ SocialApp/SignInVC.swift
@@ -7,19 +7,45 @@
 //
 
 import UIKit
+import FBSDKLoginKit
+import FBSDKCoreKit
+import Firebase
 
 class SignInVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    @IBAction func facebookButton(_ sender: Any) {
+        
+        let facebookLogin = FBSDKLoginManager()
+        
+        facebookLogin.logIn(withReadPermissions: ["email"], from: self) { (result, error) in
+            if error != nil {
+                print("BRIAN: Unable to Authenticate")
+            } else if result?.isCancelled == true {
+                print("BRIAN: User canceled Facebook authentication")
+            } else {
+                print("BRIAN: Succesfully autheticated with Facebook")
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: FBSDKAccessToken.current().tokenString)
+                self.firebaseAuth(credential)
+            }
+        
+        }
 
+    }
+    
+    func firebaseAuth(_ credential: FIRAuthCredential) {
+        FIRAuth.auth()?.signIn(with: credential, completion: { (user, error) in
+            if error != nil {
+                print("BRIAN: Unable to authenticate with Firebase")
+            } else {
+                print("BRIAN: Successfully authenticated with Firebase")
+            }
+        })
+
+    }
 
 }
-
